b1c762bc8597a128e7fb9727e7939ef8adce1429
naveen09 <aechannaveen@gmail.com>
2018-07-22 23:29:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Team Tournament web app
+
+`
+gulp serve
+`
---
+++ public/js/config/app.js
@@ -0,0 +1,20 @@
+var tournament = angular.module('tournament', ['ngMaterial', 'ngAnimate', 'ngMessages', 'ngAria', 'ui.router']);
+
+(function(app) {
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.state('home', {
+            url: '/',
+            templateUrl: 'partials/home-partial.html',
+            controller: 'HomeController'
+        })
+
+        .state('about', {
+            url: '/about',
+            templateUrl: 'partials/about-partial.html',
+            controller: 'AboutController'
+        });
+    }]);
+})(tournament);
